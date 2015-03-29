6da42c50f2ff0a432271e80339e3e2b3b7f68b04
sysxkh <sysxkh@gmail.com>
2015-03-29 18:56:54 -0400

    Parser Update
    Update Parser

--- src/main/java/edu/pitt/sis/infsci2140/gssearchengine/engine/parser/Parser.java
+++ src/main/java/edu/pitt/sis/infsci2140/gssearchengine/engine/parser/Parser.java
@@ -1,5 +1,10 @@
 package edu.pitt.sis.infsci2140.gssearchengine.engine.parser;
 
-public class Parser {
-
+public class Parser 
+{
+	private String html;
+	public Parser(String HTML)
+	{
+		this.html=HTML;
+	}
 }
